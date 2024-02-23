0c63c80763231f898b4c967ce69e354616e9e612
facostabricen90 <facostabricen90@gmail.com>
2024-02-23 14:52:38 -0500

    Agregar archivos de proyecto.

---
+++ ApiLayer/FlightsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Models;
+using BusinessLayer.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLayer
+{
+    public class FlightsController : ControllerBase
+    {
+        private readonly FlightUseCase flightUseCase;
+
+        public FlightsController(FlightUseCase flightUseCase)
+        {
+            this.flightUseCase = flightUseCase;
+        }
+
+        // GET: api/<FlightsController>
+        [HttpGet("{origin}/{destination}")]
+        public async Task<Journey> Get(string origin, string destination) => await flightUseCase.GetJourney(origin, destination);
+    }
+}
---
+++ BusinessLayer/Models/Flight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Models
+{
+    public class Flight
+    {
+        public Transport? Transport { get; set; }
+        public string? Origin { get; set; }
+        public string? Destination { get; set; }
+        public double Price { get; set; }
+
+        public Flight(Transport transport, string origin, string destination, double price)
+        {
+            this.Transport = transport;
+            this.Origin = origin;
+            this.Destination = destination;
+            this.Price = price;
+        }
+    }
+}
